b86b897f993abca681d67e1d2f17ca19eaa4cd4c
Mohammed Salman <msal4@outlook.com>
2020-11-17 02:45:55 +0300

    [add] build option to build the api for production and using bourne shell instead of bash

--- start.sh
+++ start.sh
@@ -1,8 +1,15 @@
-#!/bin/bash
+#!/bin/sh
+if [[ $1 == "--build" || $1 == "-b" ]]
+then
+  . .env
+  go build -o app .
+fi
+
 # get the db url from env vars
 URL=$(echo $DATABASE_URL | grep -o "@.*\/")
 # truncate the '@' at the start and the '/' at the end
-URL=${URL:1:-1}
+URL=${URL#?}
+URL=${URL%?}
 # wait for the db to start
 ./wait-for-it.sh $URL
 # start the app
