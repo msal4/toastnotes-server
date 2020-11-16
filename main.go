4a00827a21e58d59896e1b16016bd2167be8ca82
Mohammed Salman <msal4@outlook.com>
2020-11-16 20:53:34 +0300

    Dokku (closes #16, closes #16) (#18)
    * [improve] Dockerfile, env and start.sh to support dokku and remove docker-compose.yml

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
-	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -14,21 +12,13 @@ import (
 	"github.com/msal4/toastnotes/validation"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
-	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
 	// init
 	godotenv.Load()
-	databaseURI := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("POSTGRES_HOST"),
-		os.Getenv("POSTGRES_PORT"),
-		os.Getenv("POSTGRES_DB"),
-	)
 
-	db, err := models.OpenConnection(databaseURI, nil)
+	db, err := models.OpenConnection(os.Getenv("DATABASE_URL"), nil)
 	if err != nil {
 		panic(err)
 	}
@@ -45,23 +35,8 @@ func main() {
 	// middlewares
 	router.Use(gin.Logger())
 
-	certManager := autocert.Manager{
-		Prompt:     autocert.AcceptTOS,
-		Cache:      autocert.DirCache("cert-cache"),
-		HostPolicy: autocert.HostWhitelist("api.toast.msal.dev"),
-		Email:      "msal4@outlook.com",
-	}
-
-	server := http.Server{
-		Addr:    ":443",
-		Handler: router,
-		TLSConfig: &tls.Config{
-			GetCertificate: certManager.GetCertificate,
-		},
-	}
-
-	go http.ListenAndServe(":80", certManager.HTTPHandler(nil))
-	if err := server.ListenAndServeTLS("", ""); err != nil {
-		panic(err)
+	fmt.Println("port:", os.Getenv("PORT"))
+	if err := router.Run(); err != nil {
+		log.Fatal().Err(err)
 	}
 }
